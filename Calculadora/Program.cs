0b935a5942f6f11a3b05144225ee9f0341651188
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2023-08-01 10:56:52 -0300

    Divisao

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main (string [] args)
         {
-            Subtracao();
+            Divisao();
         }
 
         static void Soma()
@@ -41,6 +41,23 @@ namespace Calculator
            float resultado = valor1 - valor2; 
            Console.WriteLine($"O resultado da subtração é {resultado}"); 
            Console.ReadKey();           
+        }
+    
+        static void Divisao()
+        {
+           Console.Clear();
+
+           Console.WriteLine("Primeiro valor: ");
+           float valor1 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("Segundo valor: ");
+           float valor2 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("");  
+
+           float resultado = valor1 / valor2; 
+           Console.WriteLine($"O resultado da divisão é {resultado}"); 
+           Console.ReadKey(); 
         }
     }
 }
