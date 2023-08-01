699ecd3a30232be75a14e7f873bfe39ae215bdb1
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2023-08-01 10:43:13 -0300

    Subtracao

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main (string [] args)
         {
-            Soma();
+            Subtracao();
         }
 
         static void Soma()
@@ -22,7 +22,25 @@ namespace Calculator
            Console.WriteLine("");  
 
            float resultado = valor1 + valor2; 
-           Console.WriteLine($"O resultado da soma é {resultado}");            
+           Console.WriteLine($"O resultado da soma é {resultado}");  
+           Console.ReadKey();          
+        }
+    
+        static void Subtracao()
+        {
+           Console.Clear();
+
+           Console.WriteLine("Primeiro valor: ");
+           float valor1 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("Segundo valor: ");
+           float valor2 = float.Parse(Console.ReadLine());
+
+           Console.WriteLine("");  
+
+           float resultado = valor1 - valor2; 
+           Console.WriteLine($"O resultado da subtração é {resultado}"); 
+           Console.ReadKey();           
         }
     }
 }
